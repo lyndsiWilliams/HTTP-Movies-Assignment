4629dbc6cf851e53ef79188531be75e34e311114
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-18 15:53:20 -0600

    Created UpdateForm.js and added it's route in App.js

---
+++ client/src/Movies/UpdateForm.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialItem = {
+  id: Date.now(),
+  title: '',
+  director: '',
+  metascore: '',
+  stars: []
+};
+
+const UpdateForm = props => {
+  const [movie, setMovie] = useState(initialItem);
+
+  // ----- WATCH THIS!!! movie might be movies or vice versa
+  useEffect(() => {
+    const movieToEdit = props.movies.find(el => `${el.id}` === props.match.params.id);
+
+    if (movieToEdit) {
+      setMovie(movieToEdit);
+    }
+  }, [props.movies, props.match.params.id]);
+
+  const handleChange = ev => {
+    ev.preventDefault();
+
+    setMovie({ ...movie, [ev.target.name]: ev.target.value });
+  };
+
+  const handleSubmit = ev => {
+    ev.preventDefault();
+
+    axios
+      .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <h2>Update Movie</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange}
+          placeholder="title"
+          value={movie.title}
+        />
+
+        <input
+          type="text"
+          name="director"
+          onChange={handleChange}
+          placeholder="director"
+          value={movie.director}
+        />
+        
+        <input
+          type="text"
+          name="metascore"
+          onChange={handleChange}
+          placeholder="metascore"
+          value={movie.metascore}
+        />
+
+        <button>Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateForm;
